02162d996959137b45dfb08586322b28d4a7f6e9
Alex Baek <alexbaek2468@gmail.com>
2022-01-02 13:37:56 -0500

    design views are ordered as seen in the video

--- designview4/src/App.js
+++ designview4/src/App.js
@@ -2,76 +2,134 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="border">
     <div className="App">
-      <div className="bigbox">
-        <div className="navbarbig">
-          <div className="leftnavbarbig">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png" alt="" className="hamburger"/>
-            <h3 className="fintimes">FINTIMES</h3>
-          </div>
-          <div className="rightnavbarbig">
-            <img src="https://cdn0.iconfinder.com/data/icons/very-basic-2-android-l-lollipop-icon-pack/24/search-512.png" alt="" className="magnify"/>
-          </div>
-        </div>
-        <div className="main">
-          <h1 className="toptitle">Everything</h1>
-          <h1 className="midtitle">about investing plus way more.</h1>
-          <div className="mainbot">
-          <p className="description">Introducing the first all-in-one tool to help you put your financial assets in the best possible opportunities.</p>
-          <button className="signup">SIGN UP</button>
-          </div>
+      <div className="nav">
+      <a className="title"href=""><h3 className="leftside" >FLO.</h3></a>
+        <div className="navright">
+            <nav>
+              <a className="navlinks"href="">New Arrival</a>
+              <a className="navlinks"href="">Best Seller</a>
+              <a className="navlinks"href="">Reviews</a>
+              <button id="signup"className="navlinks">Sign Up</button>
+            </nav>
         </div>
       </div>
 
-      <div className="smallboxes">
-    <div className="topbox">
-      <div className="navbartop">
-        <div className="leftnavbar">
-          <h2 className="team">Team</h2>
-        </div>
-        <div className="rightnavbar">
-          <img src="https://www.pngkit.com/png/full/639-6399637_henry-circle-gentleman.png" alt="" className="person1"/>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Clinton_Circle.png" alt="" className="person2"/>
-        </div>
-      </div>
-      <div className="maintop">
-        <p className="description2">
-          CHECK OUR EXPERTS
-        </p>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Black_Right_Arrow.png" alt="" className="arrow"/>
-      </div>
-    </div>
 
-    <div className="middlebox">
-      <div className="navbarmid">
-        <div className="leftnavbar">
-          <h2 className="strategy">Strategy</h2>
+      <div className="topmain">
+        <h1>Gift and Flower for Your Beloved</h1>
+        <p className="description">Priding ourselves on the quality, bold colours and sustainability of our products, will give you a boost of confidence.</p>
+        <button className="startshopping">Start Shopping</button>
+        <div className="buttons1">
+        <div id="tulip1"className="package">
+          <img src="https://flowersgifts-ua.com/wp-content/uploads/2018/03/11-tulips.png" alt="tulipbouquet" className="tulipbouquet"/>
+          <div className="inside">
+          <h3 className="flowerpackage">Tulip Package</h3>
+          <h2 className="flowerprice">$70.99</h2>
+          <p className="detail">Detail --)</p>
+          </div>
         </div>
-        <div className="rightnavbar">
-          <button className="play"></button>
+        <div id="rose1"className="package">
+          <img src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Roses-PNG/Transparent_Red_Roses_Bouquet_PNG_Clipart_Picture.png?m=1434276736" alt="rosebouquet" className="rosebouquet" />
+          <div className="inside">
+          <h3 className="flowerpackage">Rose Package</h3>
+          <h2 className="flowerprice">$70.99</h2>
+          <p className="detail">Detail --)</p>
+          </div>
+        </div>
         </div>
       </div>
-      <div className="mainmid">
-        <p className="description2">
-          INVESTMENT PROCESS
-        </p>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Black_Right_Arrow.png" alt="" className="arrow"/>
-      </div>
-    </div>
 
-    <div className="bottombox">
-      <div className="navbarbot">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Wired_logo.svg/1280px-Wired_logo.svg.png" alt="" className="wired"/>
+
+      <div className="midbar">
+        <div className="midbardesc">
+          <h2 className="number">3000+</h2>
+          <h4 className="description2">Packages Sold</h4>
+          </div>
+        <div className="midbardesc">
+          <h2 className="number">5000+</h2>
+          <h4 className="description2">Bouquet Sold</h4>
+        </div>
+        <div className="midbardesc">
+          <h2 className="number">7000+</h2>
+          <h4 className="description2">Happy Clients</h4>
+        </div>
+        <div className="midbardesc">
+          <h2 className="number">25+</h2>
+          <h4 className="description2">Years of Experience</h4>
+        </div>
       </div>
-      <div className="mainbot2">
-        <p className="description2">OUR PARTNERS</p>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Black_Right_Arrow.png" alt="" className="arrow"/>
+      
+      
+      <div className="bottommain">
+        <div className="bottomnav">
+          <p>Product</p>
+          <div className="bottombutton">
+          <button className="bottomleft">(</button>
+          <button className="bottomright">)</button>
+            </div>
+        </div>
+        <h3 className="title2">Our Best Sellers This Month</h3>
+        <div className="bestsellers">
+          <div id="one"className="seller">
+            <div className="flag">
+            <h3 className="new">New!</h3>
+            <button className="heart"></button>
+            </div>
+            
+            <div className="sellerbottom">
+              <p className="specialnew">for special person</p>
+              <h3 className="aster">Aster Bouquet</h3>
+              <h2 className="astercost">$ 27.23</h2>
+              <div className="cartspace">
+              <button className="cart"></button>
+              </div>
+            </div>
+          </div>
+          <div id="two"className="seller">
+            <div className="flag">
+            <h3 className="new">New!</h3>
+            <button className="heart"></button>
+            </div>
+            <div className="sellerbottom">
+              <p className="specialnew">for special person</p>
+              <h3 className="aster">Rose Bouquet</h3>
+              <h2 className="astercost">$ 27.00</h2>
+              <div className="cartspace">
+              <button className="cart"></button>
+              </div>
+            </div>
+          </div>
+          <div id="three"className="seller">
+            <div className="flag2">
+            <button className="heart"></button>
+            </div>
+            <div className="sellerbottom">
+              <p className="specialnew">for special person</p>
+              <h3 className="aster">Tulip Bouquet</h3>
+              <h2 className="astercost">$ 20.99</h2>
+              <div className="cartspace">
+              <button className="cart"></button>
+              </div>
+            </div>
+          </div>
+          <div id="four"className="seller">
+            <div className="flag2">
+          <button className="heart"></button>
+          </div>
+            <div className="sellerbottom">
+              <p className="specialnew">for special person</p>
+              <h3 className="aster">Special Bouquet</h3>
+              <h2 className="astercost">$ 50.99</h2>
+              <div className="cartspace">
+              <button className="cart"></button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    </div>
     </div>
   );
 }
